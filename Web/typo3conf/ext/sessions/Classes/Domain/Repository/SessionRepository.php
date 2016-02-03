efef6cb5d7be4a5e82c01537be4813ed954394e8
Dustin Kinney <dustin@kinney.ws>
2016-02-03 23:05:06 +0100

    [TASK] SessionRepository find by multiple uids method

--- Web/typo3conf/ext/sessions/Classes/Domain/Repository/SessionRepository.php
+++ Web/typo3conf/ext/sessions/Classes/Domain/Repository/SessionRepository.php
@@ -6,4 +6,13 @@ use TYPO3\CMS\Extbase\Persistence\Repository;
 class SessionRepository extends Repository
 {
 
+    /**
+     * @param array $uids array holding multiple unique identifiers
+     * @return \TYPO3\CMS\Extbase\Persistence\QueryResultInterface
+     */
+    public function findByUids($uids)
+    {
+        $query = $this->createQuery();
+        return $query->matching($query->in('uid', $uids))->execute();
+    }
 }
