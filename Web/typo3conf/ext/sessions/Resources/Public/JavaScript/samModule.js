342f927fc2dc0c7d64886ab2252d65ba506d889c
Dustin Kinney <dustin@kinney.ws>
2016-02-05 11:51:47 +0100

    [TASK] respect json response information
    * refactored notification
    * remove unused dependencies

--- Web/typo3conf/ext/sessions/Resources/Public/JavaScript/samModule.js
+++ Web/typo3conf/ext/sessions/Resources/Public/JavaScript/samModule.js
@@ -1,4 +1,4 @@
-require(['samModuleConfig', 'jquery', 'TYPO3/CMS/Backend/Modal', 'TYPO3/CMS/Backend/Notification', 'nprogress'], function(samModuleConfig, jQuery, Modal, Notification, NProgress) {
+require(['samModuleConfig', 'jquery', 'TYPO3/CMS/Backend/Modal', 'TYPO3/CMS/Backend/Notification'], function(samModuleConfig, jQuery, Modal, Notification) {
     jQuery(document).ready(function($) {
         /*
             Show a modal without buttons when the info button is cliked inside the list
@@ -11,6 +11,18 @@ require(['samModuleConfig', 'jquery', 'TYPO3/CMS/Backend/Modal', 'TYPO3/CMS/Back
         });
 
         /*
+            Show a Notification indicating success
+         */
+        function showSessionUpdateNotification(success)
+        {
+             if(success) {
+                 Notification.success('Success', 'Session updated', 1);
+             } else {
+                 Notification.error('Error', 'Session could not be updated', 1);
+             }
+        }
+
+        /*
             Set the type of the triggered session
          */
         var updateUrl = samModuleConfig.updateUrl;
@@ -19,11 +31,15 @@ require(['samModuleConfig', 'jquery', 'TYPO3/CMS/Backend/Modal', 'TYPO3/CMS/Back
             var type = $(this).data('state');
             var url = updateUrl.replace(/%23%23%23id%23%23%23/, id);
             url = url.replace(/%23%23%23type%23%23%23/, type);
-            $.ajax(url, {context: $(this).parent('')}).done(function(data){
-                Notification.success('Success', 'Session updated', 1);
-                $(this).parents('tr.session-row').remove();
+            $.ajax(url, {context: $(this).parent(''), dataType: 'json'}).done(function(data){
+                if(data.success === true) {
+                    showSessionUpdateNotification(true);
+                    $(this).parents('tr.session-row').remove();
+                } else {
+                    showSessionUpdateNotification(false);
+                }
             }).fail(function(data){
-                Notification.error('Error', 'Session could not be updated', 1);
+                showSessionUpdateNotification(false);
             });
         });
     });
