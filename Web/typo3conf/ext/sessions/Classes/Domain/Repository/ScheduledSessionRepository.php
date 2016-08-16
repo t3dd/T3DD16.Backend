c47cbbbc3401e40df05d3174e71b25a78c719274
Sascha Nowak <sascha.nowak@netlogix.de>
2016-08-16 20:58:52 +0200

    [TASK] Add sorting

--- Web/typo3conf/ext/sessions/Classes/Domain/Repository/ScheduledSessionRepository.php
+++ Web/typo3conf/ext/sessions/Classes/Domain/Repository/ScheduledSessionRepository.php
@@ -4,4 +4,12 @@ namespace TYPO3\Sessions\Domain\Repository;
 class ScheduledSessionRepository extends AbstractSessionRepository
 {
 
+    /**
+     * @var array
+     */
+    protected $defaultOrderings = [
+        'begin' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING,
+        'highlight' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_DESCENDING
+    ];
+
 }
